30a494e0b5ff23fbd4b49bdf9feac1d6ec6deb03
Drin Duan <djq@famsungroup.com>
2021-03-01 16:01:32 +0800

    Add Redis tests

--- src/GeneralBusinessFramework/Utilities.IntegrationTests/RedisHelperIntegrationTests.cs
+++ src/GeneralBusinessFramework/Utilities.IntegrationTests/RedisHelperIntegrationTests.cs
@@ -12,12 +12,99 @@ namespace Utilities.IntegrationTests
     public class RedisHelperIntegrationTests
     {
         [Test]
-        public void SetRedisString_success()
+        public void SetRedisString_CRUD_Success()
         {
-            var stores = RedisHelperBase.Get("test1");
-            
-            
+            // Arrange
+            var key = "test_key1";
+            var value1 = "test_value_1";
+            var value2 = "test_value_2";
+
+            // Act. Init
+            var isExit = RedisHelper.IsKeyExist(key);
+            if (isExit)
+            {
+                RedisHelper.KeyDelete(key);
+            }
+
+            // Act. Add
+            RedisHelper.StringSet(key, value1);
+
+            // Assert
+            var actualValue1 = RedisHelper.StringGet(key);
+            Assert.AreEqual(value1, actualValue1);
+
+            // Act. Modify
+            RedisHelper.StringSet(key, value2);
+
+            // Assert
+            var actualValue2 = RedisHelper.StringGet(key);
+            Assert.AreEqual(value2, actualValue2);
+
+            // Act. Delete
+            RedisHelper.KeyDelete(key);
+
+            // Assert
+            Assert.IsFalse(RedisHelper.IsKeyExist(key));
+            Assert.IsNull(RedisHelper.StringGet(key));
+
         }
 
+        [Test]
+        public void SetRedisHash_CRUD_Success()
+        {
+            // Arrange
+            var key = "test_key2";
+            var object1 = new TestOrderDTO()
+            {
+                ID = 1,
+                OrderCode = "test_order_code1",
+            };
+            var object2 = new TestOrderDTO()
+            {
+                ID = 2,
+                OrderCode = "test_order_code2",
+            };
+
+            // Act. Init
+            var isExit = RedisHelper.IsKeyExist(key);
+            if (isExit)
+            {
+                RedisHelper.KeyDelete(key);
+            }
+
+            // Act. Add
+            RedisHelper.HashSet(key, "ID", object1.ID.ToString());
+            RedisHelper.HashSet(key, "OrderCode", object1.OrderCode);
+
+            // Assert
+            var actualValue1 = RedisHelper.HashGet(key, "ID");
+            var actualValue2 = RedisHelper.HashGet(key, "OrderCode");
+            Assert.AreEqual(object1.ID.ToString(), actualValue1);
+            Assert.AreEqual(object1.OrderCode, actualValue2);
+
+            // Act. Modify
+            RedisHelper.HashSet(key, "ID", object2.ID.ToString());
+            RedisHelper.HashSet(key, "OrderCode", object2.OrderCode);
+
+            // Assert
+            var actualValue3 = RedisHelper.HashGet(key, "ID");
+            var actualValue4 = RedisHelper.HashGet(key, "OrderCode");
+            Assert.AreEqual(object2.ID.ToString(), actualValue3);
+            Assert.AreEqual(object2.OrderCode, actualValue4);
+
+            // Act. Delete
+            RedisHelper.KeyDelete(key);
+
+            // Assert
+            Assert.IsFalse(RedisHelper.IsKeyExist(key));
+            Assert.IsNull(RedisHelper.StringGet(key));
+        }
+
+    }
+
+    public class TestOrderDTO
+    {
+        public int ID { get; set; }
+        public string OrderCode { get; set; }
     }
 }
--- src/GeneralBusinessFramework/Utilities/RedisHelper.cs
+++ src/GeneralBusinessFramework/Utilities/RedisHelper.cs
@@ -63,18 +63,18 @@ namespace Utilities
         public const string DefaultOrder = "desc";
 
         #region Keys
-        public static bool KeyExists(string key)
+        public static bool IsKeyExist(string key)
         {
             var bResult = Cache.KeyExists(key);
             return bResult;
         }
 
-        public static bool SetExpire(string key, DateTime datetime)
+        public static bool SetKeyExpire(string key, DateTime datetime)
         {
             return Cache.KeyExpire(key, datetime);
         }
 
-        public static bool SetExpire(string key, int timeout = 0)
+        public static bool SetKeyExpire(string key, int timeout = 0)
         {
             return Cache.KeyExpire(key, DateTime.Now.AddSeconds(timeout));
         }
@@ -91,47 +91,71 @@ namespace Utilities
 
         #endregion
 
+        #region Strings
+
+        public static bool StringSet(string key, string value, int timeout = 0)
+        {
+            bool bResult = Cache.StringSet(key, value);
+            if (timeout > 0)
+            {
+                Cache.KeyExpire(key, DateTime.Now.AddSeconds(timeout));
+            }
+            return bResult;
+        }
+
+        public static bool StringSet<T>(string key, T t, int timeout = 0)
+        {
+            var value = JsonConvert.SerializeObject(t);
+            bool bResult = Cache.StringSet(key, value);
+            if (timeout > 0)
+            {
+                Cache.KeyExpire(key, DateTime.Now.AddSeconds(timeout));
+            }
+            return bResult;
+        }
+
+        public static string StringGet(string key)
+        {
+            string value = Cache.StringGet(key);
+            return value;
+        }
+
+        public static T StringGet<T>(string key)
+        {
+            string value = Cache.StringGet(key);
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+
+        public static double StringIncrement(string key, double value)
+        {
+            return Cache.StringIncrement(key, value);
+        }
+
+        public static long StringAppend(string key, string value)
+        {
+            return Cache.StringAppend(key, value, CommandFlags.None);
+        }
+
+        #endregion
+
         #region Hashes
-        public static bool IsExist(string key, string field)
+        public static bool IsHashExist(string key, string field)
         {
             return Cache.HashExists(key, field);
         }
 
-        public static bool SetHash<T>(string key, string field, T t)
+        public static bool HashSet(string key, string field, string value)
         {
-            var value = JsonConvert.SerializeObject(t);
             return Cache.HashSet(key, field, value);
         }
 
-        public static bool Remove(string key, string field)
-        {
-            return Cache.HashDelete(key, field);
-        }
-
-        public static long StringIncrement(string key, string filed, long value = 1)
-        {
-            return Cache.HashIncrement(key, filed, value);
-        }
-
-        public static T Get<T>(string key, string filed)
-        {
-            string value = Cache.HashGet(key, filed);
-            return JsonConvert.DeserializeObject<T>(value);
-        }
-
-        public static long GetHashCount(string key)
-        {
-            var length = Cache.HashLength(key);
-            return length;
-        }
-
-        public static string Get(string key, string filed)
+        public static string HashGet(string key, string filed)
         {
             string value = Cache.HashGet(key, filed).ToString();
             return value;
         }
 
-        public static List<T> GetAll<T>(string key)
+        public static List<T> HashGetAll<T>(string key)
         {
             var result = new List<T>();
             var list = Cache.HashGetAll(key).ToList();
@@ -160,119 +184,23 @@ namespace Utilities
             }
             return result;
         }
-        #endregion
 
-        #region Sorted Sets
-        public static bool SortedSetItemIsExist(string key, string item)
+        public static bool RemoveHash(string key, string field)
         {
-            var value = GetItemScoreFromSortedSet(key, item);
-            if (value != null)
-            {
-                return true;
-            }
-            return false;
+            return Cache.HashDelete(key, field);
         }
 
-        public static bool SortedSetAdd(string key, string item, double score, int timeout = 0)
+        public static long HashIncrement(string key, string filed, long value = 1)
         {
-            return Cache.SortedSetAdd(key, item, score);
+            return Cache.HashIncrement(key, filed, value);
         }
 
-        public static List<string> GetSortedSetRangeByRank(string key, long fromRank, long toRank, string order = DefaultOrder)
+        public static long GetHashLength(string key)
         {
-            var result = new List<string>();
-            var list = Cache.SortedSetRangeByRank(key, fromRank, toRank, order == Order.Descending.ToString().ToLower() ? Order.Descending : Order.Ascending).ToList();
-            if (list.Any())
-            {
-                list.ForEach(x =>
-                {
-                    var value = JsonConvert.DeserializeObject<string>(x);
-                    result.Add(value);
-                });
-            }
-            return result;
+            var length = Cache.HashLength(key);
+            return length;
         }
 
-        public static Dictionary<string, double> GetSortedSetRangeByRankWithScores(string key, long fromRank, long toRank, string order = DefaultOrder)
-        {
-            var result = new Dictionary<string, double>();
-            var list = Cache.SortedSetRangeByRankWithScores(key, fromRank, toRank, order == Order.Descending.ToString().ToLower() ? Order.Descending : Order.Ascending).ToList();
-            if (list.Any())
-            {
-                list.ForEach(x =>
-                {
-                    result.Add(x.Element, x.Score);
-                });
-            }
-            return result;
-        }
-
-        public static List<string> GetSortedSetRangeByValue(string key, long minValue, long maxValue)
-        {
-            var result = new List<string>();
-            var list = Cache.SortedSetRangeByValue(key, minValue, maxValue).ToList();
-            if (list.Any())
-            {
-                list.ForEach(x =>
-                {
-                    var value = JsonConvert.DeserializeObject<string>(x);
-                    result.Add(value);
-                });
-            }
-            return result;
-        }
-
-        public static long GetSortedSetLength(string key)
-        {
-            return Cache.SortedSetLength(key);
-        }
-
-        public static long GetSortedSetLength(string key, double minValue, double maxValue)
-        {
-            return Cache.SortedSetLength(key, minValue, maxValue);
-        }
-
-        public static long? GetItemRankFromSortedSet(string key, string item, string order = DefaultOrder)
-        {
-            return Cache.SortedSetRank(key, item, order == Order.Descending.ToString().ToLower() ? Order.Descending : Order.Ascending);
-        }
-
-        public static double? GetItemScoreFromSortedSet(string key, string item)
-        {
-            return Cache.SortedSetScore(key, item);
-        }
-
-        public static double SetSortedSetItemIncrement(string key, string item, double score = 1)
-        {
-            return Cache.SortedSetIncrement(key, item, score);
-        }
-
-        public static double SortedSetItemDecrement(string key, string item, double score = -1)
-        {
-            return Cache.SortedSetDecrement(key, item, score);
-        }
-
-        public static bool RemoveItemFromSortedSet(string key, string item)
-        {
-            return Cache.SortedSetRemove(key, item);
-        }
-
-        public static long RemoveByRankFromSortedSet(string key, long fromRank, long toRank)
-        {
-            return Cache.SortedSetRemoveRangeByRank(key, fromRank, toRank);
-        }
-
-        public static long RemoveByScoreFromSortedSet(string key, double minValue, double maxValue)
-        {
-            return Cache.SortedSetRemoveRangeByScore(key, minValue, maxValue);
-        }
-
-        public static long RemoveByLexFromSortedSet(string key, int minValue, int maxValue)
-        {
-            //TODO： Don't know its meaning
-            //return Cache.SortedSetRemoveRangeByValue(key, minValue, maxValue);
-            return 0;
-        }
         #endregion
 
         #region Lists
@@ -299,42 +227,6 @@ namespace Utilities
         }
         #endregion
 
-        #region Strings
-
-        public static string Get(string key)
-        {
-            string value = Cache.StringGet(key);
-            return value;
-        }
-
-        public static T StringGet<T>(string key)
-        {
-            string value = Cache.StringGet(key);
-            return JsonConvert.DeserializeObject<T>(value);
-        }
-
-        public static double StringIncrement(string key, double value)
-        {
-            return Cache.StringIncrement(key, value);
-        }
-
-        public static long StringAppend(string key, string value)
-        {
-            return Cache.StringAppend(key, value, CommandFlags.None);
-        }
-
-        public static bool Set<T>(string key, T t, int timeout = 0)
-        {
-            var value = JsonConvert.SerializeObject(t);
-            bool bResult = Cache.StringSet(key, value);
-            if (timeout > 0)
-            {
-                Cache.KeyExpire(key, DateTime.Now.AddSeconds(timeout));
-            }
-            return bResult;
-        }
-
-        #endregion
     }
 
 }
